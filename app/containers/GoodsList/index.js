a5777f78fb97aa6ac1d0cb410e9b13e1713e27ff
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-08-11 00:18:33 +0300

    Refactor getGoods selector

--- app/containers/GoodsList/index.js
+++ app/containers/GoodsList/index.js
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import {connect} from 'react-redux';
 import {getGoods} from 'selectors/main';
+import {setGoods} from 'actions';
+import {bindActionCreators} from 'redux';
 
 class GoodsList extends React.Component {
   render () {
-    console.log('GoodsList render');
+    console.log('GoodsList render', this.props.goods.toJS());
     return (
       <div>
         <ul>
           {this.props.goods.map((g, i) => <li key={i}>{`${g.get('name')} - ${g.get('price')}$`}</li>)}
         </ul>
+        <button onClick={() => this.props.setGoods(this.props.goods)}>Set Goods</button>
       </div>
     )
   }
@@ -22,4 +25,8 @@ const mapStateToProps = (state) => {
   };
 }
 
-export default connect(mapStateToProps, null)(GoodsList);
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  setGoods,
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoodsList);
--- app/selectors/main.js
+++ app/selectors/main.js
@@ -1,1 +1,12 @@
-export const getGoods = (state) => state.getIn(['main', 'goods']);
+export const getGoods = (state) => {
+  const list = state.getIn(['main', 'goods']);
+  const sorted = state.getIn(['main', 'sorted']);
+
+  return sorted ? list.sort((a, b) => {
+    const aPrice = a.get('price');
+    const bPrice = b.get('price');
+    if (aPrice < bPrice) { return -1; }
+    if (aPrice > bPrice) { return 1; }
+    if (aPrice === bPrice) { return 0; }
+  }) : list;
+}
