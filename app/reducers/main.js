5f9d3c73061b593857d2100be5d098b2981e5eef
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-08-10 23:34:38 +0300

    Add console.logs to reducer

--- app/reducers/main.js
+++ app/reducers/main.js
@@ -23,12 +23,22 @@ const initialState = fromJS({
   sorted: false,
 });
 
+let prevState = initialState;
+
 export default (state = initialState, action) => {
   switch (action.type) {
-    case SET_GOODS:
-      return state.set('goods', action.goods);
-    case SET_SORTED:
-      return state.set('sorted', action.sorted);
+    case SET_GOODS: {
+      const newState = state.set('goods', action.goods);
+      console.log('SET_GOODS', prevState === newState);
+      prevState = newState;
+      return newState;
+    }
+    case SET_SORTED: {
+      const newState = state.set('sorted', action.sorted);
+      console.log('SET_SORTED', prevState === newState);
+      prevState = newState;
+      return newState;
+    }
     default:
       return initialState;
   }
